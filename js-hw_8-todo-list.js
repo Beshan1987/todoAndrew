174626ff44a346733b3fbc52f1f26d35bcaa1399
Beshan1987 <117032614+Beshan1987@users.noreply.github.com>
2023-02-21 15:31:11 +0300

    Update js-hw_8-todo-list.js

--- js-hw_8-todo-list.js
+++ js-hw_8-todo-list.js
@@ -151,7 +151,7 @@ function checkTask(task) {
 						<button type="button" data-action="delete" class="btn-action">
 							<img src="./img/cross.svg" alt="Done" width="18" height="18">
 						</button>
-						<span class="badge bg-light text-dar middle ${cssClass}">${date}</span>						
+						<span class="badge text-dar middle ${cssClass}">${date}</span>						
 					</div>
 				</li>`;
 	tasksList.insertAdjacentHTML("beforeend", taskHTML);
